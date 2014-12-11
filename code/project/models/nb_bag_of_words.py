0b3ef79cc26963ab35aa1e9ab8159f698e099854
Stuart Wagner <wagners@plus.seas.upenn.edu>
2014-12-10 21:04:44 -0500

    implemented predict

--- code/project/models/nb_bag_of_words.py
+++ code/project/models/nb_bag_of_words.py
@@ -32,6 +32,9 @@ def train(train_files, train_labels):
 
     n = len(keys)
     m = len(ob_ids)
+    print n
+    print m
+    print len(keys_indices), "keys indices"
 
     # Observations
     X = np.zeros((m,n), dtype=np.int)
@@ -48,11 +51,33 @@ def train(train_files, train_labels):
 
     gnb = GaussianNB()
     gnb.fit(X, Y)
+    print X.shape
 
-    return gnb
+    return (gnb, keys_indices, tokens_by_file)
 
 
 def predict(model, test_files):
     """
     """
-    return {}
+    keys_indices = model[1]
+    tokens_by_file = model[2]
+    model = model[0]
+    tokens_by_file = project.CoreNLP.tokenize_keys(test_files)
+    ob_ids = filename_to_id(test_files)
+    # Strip all the junk:
+    corpus = strip_non_words(chain.from_iterable(tokens_by_file.values()))
+    counts = Counter(corpus)
+
+    n = len(keys_indices)
+    m = len(ob_ids)
+
+    # Observations
+    X = np.zeros((m,n), dtype=np.int)
+    for (i,ob_id) in enumerate(ob_ids, start=0):
+        for token in strip_non_words(tokens_by_file[ob_id]):
+            if token in keys_indices:
+            	j = keys_indices[token]
+            	X[i][j] = 1
+    print X.shape
+    print model.predict(X)
+    return model.predict(X)
